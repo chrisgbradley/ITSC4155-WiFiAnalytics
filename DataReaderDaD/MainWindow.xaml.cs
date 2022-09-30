c05cd07a499cafa9879d17c0c570c3d96fc8c2a1
Christian Bradley <chris@chrisgbradley.com>
2022-09-30 03:41:19 -0400

    freezing while reading documents, required overhaul to use backgroundworker

--- DataReaderDaD/MainWindow.xaml.cs
+++ DataReaderDaD/MainWindow.xaml.cs
@@ -14,6 +14,8 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace DataReaderDaD
 {
@@ -22,66 +24,196 @@ namespace DataReaderDaD
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        private readonly Regex regex = new Regex(@"(?<DATE>[a-zA-Z]+\s+(0?[1-9]|[12][0-9]|3[01]))\s+"
+                                        + @"(?<TIME>[0-9]{2}:[0-9]{2}:[0-9]{2})\s(?<HOST>\S+)\s+"
+                                        + @"(?<PROCNAME>[a-zA-Z0-9\-]+)\[(?<PID>[^\]]*)\]:\s+"
+                                        + @"<(?<LOGCODE>[0-9]{6})>\s+(?:<(\d*)>\s+)?"
+                                        + @"<(?<TYPE>[a-zA-Z]+)>");
+
+        private readonly BackgroundWorker backgroundWorker = new BackgroundWorker();
+
+        private void InitializeBackgroundWorker()
+        {
+            // Background Process
+            backgroundWorker.DoWork += BackgroundWorker_DoWork;
+            backgroundWorker.RunWorkerCompleted += BackgroundWorker_RunWorkerCompleted;
+
+            // Progress Reporting
+            backgroundWorker.WorkerReportsProgress = true;
+            backgroundWorker.ProgressChanged += BackgroundWorker_ProgressChanged;
+
+            // Cancellation
+            backgroundWorker.WorkerSupportsCancellation = true;
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            InitializeBackgroundWorker();// set BGworker config
         }
+
 
         private void MainWindow1_DragEnter(object sender, DragEventArgs e)
         {
-            // Basically used to prime the window for dragging and dropping.
+            // Used to prime the window for dragging and dropping.
             e.Effects = DragDropEffects.All;
         }
 
+
         private void MainWindow1_Drop(object sender, DragEventArgs e)
         {
-            // Creates a string array to collect lines from a document.
-            string[] getItems = (string[]) e.Data.GetData(DataFormats.FileDrop, false);
+
+            // dont allow drops while parsing
+            if (backgroundWorker.IsBusy)
+                return;
+
+            // file paths of files dropped in
+            string[] filesThatWereDropped = (string[])(e.Data.GetData(DataFormats.FileDrop));
+
+            // add all paths to listbox in GUI
+            foreach(string file in filesThatWereDropped)
+                FilesNamesDropped.Items.Add(file);
+
+
+        }
+
+
+        // Runs on UI Thread
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            // protect start button while parsing
+            if (backgroundWorker.IsBusy)
+                return;
+
+            // get file paths from listbox in GUI
+            string[] filesThatWereDropped = FilesNamesDropped.Items.OfType<string>().ToArray();
+
+            // start parsing
+            backgroundWorker.RunWorkerAsync(filesThatWereDropped);
+
+            StartButton.IsEnabled = !backgroundWorker.IsBusy;//disable start button while parsing
+            CancelButton.IsEnabled = backgroundWorker.IsBusy;//enable cancel button while parsing
+            OutputTextBox.Content = string.Empty; //prep output label
+        }
+
+        // Runs on UI Thread
+        private void CancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            backgroundWorker.CancelAsync(); // send cancellation request to worker
+        }
+
+
+        private void BackgroundWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Do not access the form's BackgroundWorker reference directly.
+            // Instead, use the reference provided by the sender parameter.
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            // get file paths that were passed
+            string[] filesThatWereDropped = (string[])e.Argument;
+
             
-            //  Creats a string list to store each line from the string array.
-            List<string> lines = File.ReadAllLines(getItems[0]).ToList();
-
-            /*
-            This was the source of my problem.  Is there a way to break a Regex pattern into separate lines?
-            Regex regex = new Regex(@"(?<DATE>[a-zA-Z]+\s+(0?[1-9]|[12][0-9]|3[01]))\s+
-                                      (?<TIME>[0-9]{2}:[0-9]{2}:[0-9]{2})\s(?<HOST>\S+)\s+
-                                      (?<PROCNAME>[a-zA-Z0-9\-]+)\[(?<PID>[^\]]*)\]:\s+
-                                     <(?<LOGCODE>[0-9]{6})>\s+(?:<(\d*)>\s+)?<(?<TYPE>[a-zA-Z]+)>");
-            */
-            Regex regex = new Regex(@"(?<DATE>[a-zA-Z]+\s+(0?[1-9]|[12][0-9]|3[01]))\s+(?<TIME>[0-9]{2}:[0-9]{2}:[0-9]{2})\s(?<HOST>\S+)\s+(?<PROCNAME>[a-zA-Z0-9\-]+)\[(?<PID>[^\]]*)\]:\s+<(?<LOGCODE>[0-9]{6})>\s+(?:<(\d*)>\s+)?<(?<TYPE>[a-zA-Z]+)>");
-
-            // Foreach loop that will read every line from the file that is now stored in a list.
-            foreach (string line in lines) 
+            ulong totalSizeInBytes = 0;
+            foreach (string file in filesThatWereDropped)
             {
-                // Used to match each line with the regex pattern from above.
-                Match match = regex.Match(line);
-                
-                // Can't get this to only print matched variables.
-                // Problem solved.
-                // If the match is successful, everyting within the if statement will be performed.
-                if (match.Success)
-                {
-                    //  Regex group matches being converted to variables that will eventually be sent to a database.
-                    string date = match.Groups["DATE"].Value;
-                    string time = match.Groups["TIME"].Value;
-                    string host = match.Groups["HOST"].Value;
-                    string procname = match.Groups["PROCNAME"].Value;
-                    int pid = Convert.ToInt32(match.Groups["PID"].Value);
-                    int logcode = Convert.ToInt32(match.Groups["LOGCODE"].Value);
-                    string type = match.Groups["TYPE"].Value;
-
-                    // A string that displays all of the variable from the regex group patterns.
-                    string logLine = "Date: " + date + " Time: " + time + 
-                                     " Host: " + host + " Proc. Name: " + procname + 
-                                     " PID: " + pid +" Log Code: " + logcode + " Type: " + type;
-                    
-                    // The string with the variables in it being sent to the list box for display.
-                    DragDrobListbox.Items.Add(logLine);
-                }
-                
-                // This was used to make sure that the drag and drop was working.
-                //DragDrobListbox.Items.Add(line);
+                FileInfo fileInfo = new FileInfo(file);
+                totalSizeInBytes += (ulong)fileInfo.Length;
             }
+
+            ulong bytesReadSoFar = 0;
+            ulong bytesLastReportedAt = 0;
+            ulong reportIntervalInBytes = 8192;
+
+            foreach (string file in filesThatWereDropped)
+            {
+                // provides auto try catch block and inits reader for you
+                using (StreamReader reader = new StreamReader(file)) 
+                {
+                    while (reader.Peek() >= 0) // until end of file
+                    {
+                        if (worker.CancellationPending)
+                        {
+                            e.Cancel = true;
+                            break;
+                        }
+
+                        string line = reader.ReadLine();
+
+                        // not sure if this is exactly correct because im lazy
+                        // taking the character length of the string and converting to bytes
+                        bytesReadSoFar += (ulong) line.Length * sizeof(Char); 
+
+                        Match match = regex.Match(line);
+
+                        if (match.Success)
+                        {
+                            // @TODO: Remove this line. Only for debugging purposes. Replace with file write or database put
+                            Debug.WriteLine(parseMatch(match));
+
+                            // only report progress if reportInterval has been passed
+                            if(bytesReadSoFar - bytesLastReportedAt > reportIntervalInBytes)
+                            {
+                                int percentageComplete = (int)((float)bytesReadSoFar / totalSizeInBytes * 100);
+                                string progressMessage = $"Read {bytesReadSoFar / 1024} KB of {totalSizeInBytes / 1024} KB";
+                                worker.ReportProgress(percentageComplete, progressMessage);
+                                bytesLastReportedAt = bytesReadSoFar; // reset report interval
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+        private void BackgroundWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                OutputTextBox.Content = e.Error.Message; // @TODO: this almost definitely will not work
+            }
+            else if (e.Cancelled)
+            {
+                OutputTextBox.Content = "Canceled";
+            }
+            else
+            {
+                OutputTextBox.Content = e.Result?.ToString(); // @TODO: this won't work either
+                DataProcessingProgress.Value = 0;
+            }
+            StartButton.IsEnabled = !backgroundWorker.IsBusy; // enable start button
+            CancelButton.IsEnabled = backgroundWorker.IsBusy; // disable cancel button
+        }
+
+
+        private void BackgroundWorker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            DataProcessingProgress.Value = e.ProgressPercentage;
+            OutputTextBox.Content = e.UserState?.ToString(); // pass progressmessage from worker to UI thread
+        }
+
+
+        private string parseMatch(Match match)
+        {
+            //  Regex group matches being converted to variables that will eventually be sent to a database.
+            string date = match.Groups["DATE"].Value;
+            string time = match.Groups["TIME"].Value;
+            string host = match.Groups["HOST"].Value;
+            string procname = match.Groups["PROCNAME"].Value;
+            int pid = Convert.ToInt32(match.Groups["PID"].Value);
+            int logcode = Convert.ToInt32(match.Groups["LOGCODE"].Value);
+            string type = match.Groups["TYPE"].Value;
+
+            // A string that displays all of the variable from the regex group patterns.
+            string parsedLogLine = "Date: " + date
+                             + " Time: " + time
+                             + " Host: " + host
+                             + " Proc. Name: " + procname
+                             + " PID: " + pid
+                             + " Log Code: " + logcode
+                             + " Type: " + type;
+
+            return parsedLogLine;
         }
     }
 }
