988e5beef6218446f3a19a29c07012aa8024c174
Emily Ruttenberg <emily.ruttenberg@gmail.com>
2022-10-23 17:39:16 -0400

    Basic html/css formatting.

---
+++ NinerFiAnalytics/Pages/Charts.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NinerFiAnalytics.Pages
+{
+    public class ChartsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
